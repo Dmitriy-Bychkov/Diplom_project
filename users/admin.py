24c14f0dff77dcf7d3626267bdcf8776d018aa06
Dmitriy Bychkov <582620@gmail.com>
2023-11-23 16:26:31 +0800

    Rewrite admin presentation for users

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ Представление пользователей в админке """
+
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email', 'phone', 'country',)
