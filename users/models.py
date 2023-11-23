4f0cbe025ec576f9230f37743dab5e520aca8d7f
Dmitriy Bychkov <582620@gmail.com>
2023-11-23 16:07:52 +0800

    Create users model with migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from education.models import NULLABLE
+
+
+class User(AbstractUser):
+    """ Модель для пользователей сервиса """
+
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='почта')
+    avatar = models.ImageField(upload_to='users/', default='no_image.jpg', verbose_name='аватар', **NULLABLE)
+    phone = models.CharField(unique=True, max_length=20, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        """ Представление написания заголовков в админке """
+
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+
+
+app_name = UsersConfig.name
+
+
+
+urlpatterns = [
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
