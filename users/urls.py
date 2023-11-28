4369255850ad1592f0c1e1bc1ea64fe25c036b59
Dmitriy Bychkov <582620@gmail.com>
2023-11-28 21:38:15 +0800

    Add urls for the users app

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
+from users.views import LoginView, LogoutView, RegisterView, UserUpdateView, set_new_password, \
+    UserConfirmEmailView, \
+    UserConfirmedView, password_reset, UserConfirmationSentView, UserListView
 
-from users.apps import UsersConfig
-
-
-app_name = UsersConfig.name
-
-
+app_name = "users"
 
 urlpatterns = [
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path("", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("registration/", RegisterView.as_view(), name="registration"),
+    path('email_confirmation_sent/', UserConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email_confirmed/', UserConfirmedView.as_view(), name='email_confirmed'),
+    path("profile/", UserUpdateView.as_view(), name="profile"),
+    path("profile/set_password", set_new_password, name="set_password"),
+    path('password_reset/', password_reset, name='password_reset'),
+    path('users_list/', UserListView.as_view(), name='users_list'),
 ]
