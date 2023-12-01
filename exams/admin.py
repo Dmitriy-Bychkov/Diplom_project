7d837a6751e22d2f27dd0eefdfbaf19e370349f9
Dmitriy Bychkov <582620@gmail.com>
2023-12-01 11:26:48 +0800

    Set django-admin through inlines

--- exams/admin.py
+++ exams/admin.py
@@ -3,20 +3,21 @@ from django.contrib import admin
 from exams.models import Answer, QuestionAnswer, Question, ExamQuestion, Exam
 
 
+class QuestionAnswerInlineAdmin(admin.TabularInline):
+    """ Представление инлайн ответов """
+
+    fields = ('question', 'answer', 'is_correct')
+    extra = 1
+    model = QuestionAnswer
+
+
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
     """ Представление ответов на тесты в админке """
 
     list_display = ('text',)
     search_fields = ('text',)
-
-
-@admin.register(QuestionAnswer)
-class QuestionAnswerAdmin(admin.ModelAdmin):
-    """ Представление ответов на тесты в админке """
-
-    list_display = ('question', 'answer', 'is_correct')
-    search_fields = ('is_correct',)
+    inlines = [QuestionAnswerInlineAdmin]
 
 
 @admin.register(Question)
@@ -26,13 +27,15 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('text',)
     search_fields = ('text',)
 
+    inlines = [QuestionAnswerInlineAdmin]
 
-@admin.register(ExamQuestion)
-class ExamQuestionAdmin(admin.ModelAdmin):
+
+class ExamQuestionInlineAdmin(admin.TabularInline):
     """ Представление ответов на тесты в админке """
 
-    list_display = ('test', 'question')
-    search_fields = ('test',)
+    fields = ('test', 'question')
+    extra = 1
+    model = ExamQuestion
 
 
 @admin.register(Exam)
@@ -41,3 +44,5 @@ class ExamAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'material')
     search_fields = ('name',)
+
+    inlines = [ExamQuestionInlineAdmin]
