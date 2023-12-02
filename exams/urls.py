1b53c99a23eaaced1aab826e758d2868ed3c79e4
Dmitriy Bychkov <582620@gmail.com>
2023-12-02 22:48:05 +0800

    Add exam and exam_result pages and its controllers + url

--- exams/urls.py
+++ exams/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from exams.apps import ExamsConfig
+from exams.views import ExamView, ExamResultsView
 
 app_name = ExamsConfig.name
 
 urlpatterns = [
-    # path('', IndexView.as_view(), name='index'),
+    path('exam/<int:exam_id>/', ExamView.as_view(), name='exam'),
+    path('exam/<int:exam_id>/results/', ExamResultsView.as_view(), name='exam_results'),
 ]
--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from exams.models import Exam
+from exams.services import calculate_exam_results
 
-# Create your views here.
+
+class ExamView(View):
+    """ Контроллер для отображения страницы с экзаменом """
+
+    def get(self, request, exam_id):
+        """
+        Обработчик GET-запроса для отображения страницы с экзаменом.
+
+        Args:
+            request (HttpRequest): HTTP-запрос.
+            exam_id (int): Идентификатор экзамена.
+
+        Returns:
+            HttpResponse: HTTP-ответ с отображением страницы экзамена.
+        """
+
+        exam = get_object_or_404(Exam, pk=exam_id)
+        questions = exam.questions.all()
+
+        return render(request, 'exams/exam.html', {'exam': exam, 'questions': questions})
+
+
+class ExamResultsView(View):
+    """ Контроллер для отображения результатов экзамена """
+
+    def post(self, request, exam_id):
+        """
+        Обработчик POST-запроса для расчета и отображения результатов экзамена.
+
+        Args:
+            request (HttpRequest): HTTP-запрос.
+            exam_id (int): Идентификатор экзамена.
+
+        Returns:
+            HttpResponse: HTTP-ответ с результатами экзамена.
+        """
+
+        exam = get_object_or_404(Exam, pk=exam_id)
+        questions = exam.questions.all()
+
+        # Вызов функции calculate_exam_results из services.py для расчета результатов экзамена
+        context = calculate_exam_results(request, questions, exam_id)
+
+        return render(request, 'exams/exam_results.html', context)
